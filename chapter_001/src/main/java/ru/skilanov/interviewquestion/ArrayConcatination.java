c9f3446c3c774c938810ffeeb821073b68577881
Kilanov <semyon.kilanov@gmail.com>
2016-11-03 23:26:48 +0500

    Chapter_01, InterviewQuestion and InterviewQuestion classes was modifided

--- chapter_001/src/main/java/ru/skilanov/interviewquestion/ArrayConcatination.java
+++ chapter_001/src/main/java/ru/skilanov/interviewquestion/ArrayConcatination.java
@@ -2,13 +2,31 @@ package ru.skilanov.interviewquestion;
 
 public class ArrayConcatination {
     public int[] concatination(int[] two, int[] one) {
-        int[] three = new int[two.length + one.length];
-        if (one[0] > two[0]) {
-            System.arraycopy(one, 0, three, 0, one.length);
-            System.arraycopy(two, 0, three, one.length, two.length);
-        } else {
-            System.arraycopy(two, 0, three, 0, two.length);
-            System.arraycopy(one, 0, three, two.length, one.length);
+        int[] three = new int[one.length + two.length];
+        int i = 0;
+        int j = 0;
+        int index = 0;
+        while (i < one.length && j < two.length) {
+            if (one[i] < two[j]) {
+                three[index] = one[i];
+                i++;
+            } else {
+                three[index] = two[j];
+                j++;
+            }
+
+            index++;
+        }
+        while (i < one.length) {
+            three[index] = one[i];
+            index++;
+            i++;
+        }
+
+        while (j < two.length) {
+            three[index] = two[j];
+            index++;
+            j++;
         }
         return three;
     }
--- chapter_001/src/test/java/ru/skilanov/interviewquestion/ArrayConcatinationTest.java
+++ chapter_001/src/test/java/ru/skilanov/interviewquestion/ArrayConcatinationTest.java
@@ -9,20 +9,20 @@ public class ArrayConcatinationTest {
     @Test
     public void whenSetFirstLessThanSecondArrayInConcatenationThenReturnSortedArray() {
         ArrayConcatination array = new ArrayConcatination();
-        int[] one = new int[]{3, 2, 1};
-        int[] two = new int[]{9, 8, 4};
+        int[] one = new int[]{1, 2, 3};
+        int[] two = new int[]{4, 8, 9};
         int[] result = array.concatination(one, two);
-        int[] expected = new int[]{9, 8, 4, 3, 2, 1};
+        int[] expected = new int[]{1, 2, 3, 4, 8, 9};
         assertThat(result, is(expected));
     }
 
     @Test
     public void whenSetSecondLessThanFirstArrayInConcatenationThenReturnSortedArray() {
         ArrayConcatination array = new ArrayConcatination();
-        int[] one = new int[]{9, 8, 4};
-        int[] two = new int[]{3, 2, 1};
+        int[] one = new int[]{4, 8, 9};
+        int[] two = new int[]{1, 2, 3};
         int[] result = array.concatination(one, two);
-        int[] expected = new int[]{9, 8, 4, 3, 2, 1};
+        int[] expected = new int[]{1, 2, 3, 4, 8, 9};
         assertThat(result, is(expected));
     }
 
