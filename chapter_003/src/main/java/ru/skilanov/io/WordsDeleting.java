a48d495f7c1b1b6deed3839983c2a4d4d87989c3
Kilanov <semyon.kilanov@gmail.com>
2017-04-18 19:06:38 +0500

    Chapter_003, lesson 2 was added

--- chapter_003/src/main/java/ru/skilanov/io/WordsDeleting.java
+++ chapter_003/src/main/java/ru/skilanov/io/WordsDeleting.java
@@ -1,7 +1,36 @@
 package ru.skilanov.io;
 
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.OutputStream;
+import java.io.IOException;
 /**
- * Created by Semen on 15.04.2017.
+ * This class drops words.
  */
 public class WordsDeleting {
+    /**
+     * This method drops words.
+     *
+     * @param in    InputStream
+     * @param out   OutputStream
+     * @param abuse String[]
+     */
+    public void dropAbuses(InputStream in, OutputStream out, String[] abuse) {
+        try (BufferedReader inReader = new BufferedReader(new InputStreamReader(in));
+             BufferedWriter outReader = new BufferedWriter(new OutputStreamWriter(out))) {
+            while (inReader.ready()) {
+                String inline = inReader.readLine();
+                for (String str : abuse) {
+                    inline = inline.replace(str, "");
+                }
+                outReader.write(inline);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- chapter_003/src/test/java/ru/skilanov/ioTest/WordsDeletingTest.java
+++ chapter_003/src/test/java/ru/skilanov/ioTest/WordsDeletingTest.java
@@ -1,7 +1,35 @@
 package ru.skilanov.ioTest;
 
+import org.junit.Test;
+import ru.skilanov.io.WordsDeleting;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
 /**
- * Created by Semen on 15.04.2017.
+ * This is test for WordsDeleting class.
  */
 public class WordsDeletingTest {
+    /**
+     * This method checking dropAbuse method.
+     */
+    @Test
+    public void whenSetNumThenReturnTrue() {
+        WordsDeleting deleting = new WordsDeleting();
+        String checkingArray = "My name is Java";
+        String[] abuse = {"a", "e", "y", "i"};
+        String resultArray = "M nm s Jv";
+
+        try (OutputStream outputStream = new ByteArrayOutputStream()) {
+            deleting.dropAbuses(new ByteArrayInputStream(checkingArray.getBytes()), outputStream, abuse);
+            assertThat(resultArray, is(outputStream.toString()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
