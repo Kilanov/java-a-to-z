67331d50aaddee1decfd01805008203bf17b8abe
Kilanov <semyon.kilanov@gmail.com>
2017-04-23 14:48:54 +0500

    Chapter_003, lesson 3 was added

---
+++ chapter_003/src/main/java/ru/skilanov/io/FileSorter.java
@@ -0,0 +1,7 @@
+package ru.skilanov.io;
+
+/**
+ * Created by Semen on 16.04.2017.
+ */
+public class FileSorter {
+}
---
+++ chapter_003/src/test/java/ru/skilanov/ioTest/FileSorterTest.java
@@ -0,0 +1,7 @@
+package ru.skilanov.ioTest;
+
+/**
+ * Created by Semen on 23.04.2017.
+ */
+public class FileSorterTest {
+}
