37126ba03b0179d92cdc4c6cc28780697bc28472
Kilanov <semyon.kilanov@gmail.com>
2017-04-23 14:49:30 +0500

    Chapter_003, lesson 3 was added

--- chapter_003/src/main/java/ru/skilanov/io/FileSorter.java
+++ chapter_003/src/main/java/ru/skilanov/io/FileSorter.java
@@ -1,7 +1,62 @@
 package ru.skilanov.io;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
 /**
- * Created by Semen on 16.04.2017.
+ * This class sorts file.
  */
 public class FileSorter {
+
+    /**
+     * This method works with files.
+     *
+     * @param source   File
+     * @param distance File
+     * @throws IOException
+     */
+    public void sort(File source, File distance) {
+        ArrayList<String> list = new ArrayList<>();
+        try (RandomAccessFile reader = new RandomAccessFile(source, "r");
+             RandomAccessFile writer = new RandomAccessFile(distance, "rw")) {
+            try {
+                String lineTemp = reader.readLine();
+                while (lineTemp != null) {
+                    list.add(lineTemp);
+                    lineTemp = reader.readLine();
+                }
+                list = arraySorter(list);
+                for (String tempString : list) {
+                    writer.write(tempString.getBytes());
+                    writer.write(System.lineSeparator().getBytes());
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * This method sorts array.
+     *
+     * @param list ArrayList
+     * @return ArrayList
+     */
+    public ArrayList arraySorter(ArrayList list) {
+        Collections.sort(list,
+                new Comparator<String>() {
+                    public int compare(String o1, String o2) {
+                        return o1.length() - o2.length();
+                    }
+                }
+        );
+        return list;
+    }
 }
+
--- chapter_003/src/test/java/ru/skilanov/ioTest/FileSorterTest.java
+++ chapter_003/src/test/java/ru/skilanov/ioTest/FileSorterTest.java
@@ -1,7 +1,25 @@
 package ru.skilanov.ioTest;
 
+import org.junit.Assert;
+import org.junit.Test;
+import ru.skilanov.io.FileSorter;
+
+import java.io.File;
+
 /**
- * Created by Semen on 23.04.2017.
+ * This is test for FileSorter class.
  */
 public class FileSorterTest {
+    /**
+     * This is test that compares two files.
+     */
+    @Test
+    public void whenWeSortFileThenReturnSortedFile() {
+        File source = new File("D:\\test.txt");
+        File distance = new File("D:\\test2.txt");
+        FileSorter fs = new FileSorter();
+        fs.sort(source, distance);
+
+        Assert.assertFalse(source.equals(distance));
+    }
 }
