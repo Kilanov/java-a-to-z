78daa4467a657d5e6e9d2470e5625b4d25566af3
Kilanov <semyon.kilanov@gmail.com>
2017-10-17 14:02:39 +0500

    Chapter_003 3, lesson 2, try with resources was added

--- chapter_003/src/main/java/ru/skilanov/io/Client.java
+++ chapter_003/src/main/java/ru/skilanov/io/Client.java
@@ -1,20 +1,75 @@
 package ru.skilanov.io;
 
-import java.io.*;
+
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.InetAddress;
 import java.net.Socket;
 
 /**
- * Created by Semen on 15.06.2017.
+ * This is the socket client class.
  */
 public class Client {
+
+    /**
+     * This is IP.
+     */
+    public static final String IP = "127.0.0.1";
+
+    /**
+     * This is port.
+     */
+    public static final int INT = 5000;
+
+    /**
+     * This is app dialog var.
+     */
+    public static final String CONNECTING_TO = "Connecting to ";
+
+    /**
+     * This is app dialog var.
+     */
+    public static final String PLEASE_INPUT_THE_MESSAGE = "Please input the message";
+
+    /**
+     * This is word for exit from app.
+     */
+    public static final String EXIT = "exit";
+
+    /**
+     * This is app dialog var.
+     */
+    public static final String ORACL = "Oracl: ";
+
+    /**
+     * This is app dialog var.
+     */
+    public static final String ME = "Me: ";
+
+    /**
+     * This is main method.
+     *
+     * @param args String[]
+     */
     public static void main(String[] args) {
-        int port = 5000;
-        String ip = "127.0.0.1";
+        Client client = new Client();
+        client.clientAction();
+    }
+
+    /**
+     * This is client method.
+     */
+    public void clientAction() {
+        int port = INT;
+        String ip = IP;
 
         try {
             InetAddress inetAddress = InetAddress.getByName(ip);
-            System.out.println("Connecting to " + port);
+            System.out.println(CONNECTING_TO + port);
             Socket socket = new Socket(inetAddress, port);
 
             InputStream socketInput = socket.getInputStream();
@@ -25,18 +80,18 @@ public class Client {
 
             BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
             String str = null;
-            System.out.println("Please input the message");
+            System.out.println(PLEASE_INPUT_THE_MESSAGE);
 
             while (true) {
                 str = reader.readLine();
-                if (str.equals("exit")){
+                if (str.equals(EXIT)) {
                     break;
-                }else {
+                } else {
                     out.writeUTF(str);
                     out.flush();
                     str = in.readUTF();
-                    System.out.println("Oracl: " + str);
-                    System.out.print("Me: ");
+                    System.out.println(ORACL + str);
+                    System.out.print(ME);
                 }
             }
         } catch (Exception e) {
--- chapter_003/src/main/java/ru/skilanov/io/Server.java
+++ chapter_003/src/main/java/ru/skilanov/io/Server.java
@@ -1,6 +1,13 @@
 package ru.skilanov.io;
 
-import java.io.*;
+
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
@@ -8,17 +15,71 @@ import java.util.List;
 import java.util.Random;
 
 /**
- * Created by Semen on 15.06.2017.
+ * This is the socket server class.
  */
 public class Server {
+
+    /**
+     * This is constant variable for port.
+     */
+    public static final int PORT_NUMBER = 5000;
+    /**
+     * This is app dialog var.
+     */
+    public static final String LOOKING_FORWARD_TO_CONNECTION = "Looking forward to connection";
+    /**
+     * This is app dialog var.
+     */
+    public static final String DONE = "Done";
+    /**
+     * This is file.
+     */
+    public static final String D_FILE_TXT = "D:/file.txt";
+    /**
+     * This is app dialog var.
+     */
+    public static final String MESSAGE_FROM_USER = "Message from user: ";
+    /**
+     * This is app dialog var.
+     */
+    public static final String SENDING_AN_ANSWER = "Sending an answer ";
+
+    /**
+     * This is constant.
+     */
+    private final Socket socket;
+
+    /**
+     * This is constructor.
+     *
+     * @param socket Socket
+     */
+    public Server(Socket socket) {
+        this.socket = socket;
+    }
+
+    /**
+     * This is main method.
+     *
+     * @param args String[]
+     */
     public static void main(String[] args) {
-        int port = 5000;
+        try {
+            Socket socket = new ServerSocket(PORT_NUMBER).accept();
+            Server server = new Server(socket);
+            server.serverAction();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    /**
+     * This is server method.
+     */
+    public void serverAction() {
         try {
             Random random = new Random();
-            ServerSocket serverSocket = new ServerSocket(port);
-            System.out.println("looking for connection");
-            Socket socket = serverSocket.accept();
-            System.out.println("Done");
 
             InputStream socketInput = socket.getInputStream();
             OutputStream socketOutput = socket.getOutputStream();
@@ -27,17 +88,17 @@ public class Server {
             DataOutputStream out = new DataOutputStream(socketOutput);
 
             List<String> wordsList = new ArrayList<>();
-            FileInputStream fstream = new FileInputStream("D:/file.txt");
-            BufferedReader br = new BufferedReader(new InputStreamReader(fstream));
+            FileInputStream fStream = new FileInputStream(D_FILE_TXT);
+            BufferedReader br = new BufferedReader(new InputStreamReader(fStream));
             String s;
-            while((s = br.readLine()) != null) {
+            while ((s = br.readLine()) != null) {
                 wordsList.add(s);
             }
 
             while (true) {
-                String str = str = in.readUTF();
-                System.out.println("Message from user: " + str);
-                System.out.println("Sending an answer ");
+                String str = in.readUTF();
+                System.out.println(MESSAGE_FROM_USER + str);
+                System.out.println(SENDING_AN_ANSWER);
                 out.writeUTF(wordsList.get(random.nextInt(wordsList.size())));
                 out.flush();
             }
