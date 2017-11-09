78659db1457da09243e9faee7862337ceabf67e6
Kilanov <semyon.kilanov@gmail.com>
2017-11-09 14:46:01 +0500

    Chapter_004 lesson 1, task 3 was added.

---
+++ chapter_004/src/main/java/ru/skilanov/io/ConvertList.java
@@ -0,0 +1,53 @@
+package ru.skilanov.io;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * This class converts array to list and list to array.
+ */
+public class ConvertList {
+
+    /**
+     * This method converts array to list.
+     *
+     * @param array int[][]
+     * @return List
+     */
+    public List<Integer> toList(int[][] array) {
+        List<Integer> list = new ArrayList<>();
+
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                list.add(array[i][j]);
+            }
+        }
+
+        return list;
+    }
+
+    /**
+     * This method converts list to array.
+     *
+     * @param list List
+     * @param rows int
+     * @return array[][]
+     */
+    public int[][] toArray(List<Integer> list, int rows) {
+        Iterator<Integer> iterator = list.iterator();
+        int columns = list.size() / rows + (list.size() % rows == 0 ? 0 : 1);
+
+        int[][] array = new int[rows][columns];
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                if (iterator.hasNext()) {
+                    array[i][j] = iterator.next();
+                } else {
+                    array[i][j] = 0;
+                }
+            }
+        }
+        return array;
+    }
+}
