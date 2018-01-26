d080c4e0384fa61d6b5909baa4c7a32b5e4e9672
Kilanov <semyon.kilanov@gmail.com>
2018-01-26 17:48:18 +0500

    Chapter_005, lesson 3, part 4 was added additional test case.

--- chapter_005/src/test/java/ru/skilanov/io/list/CyclingLinkedListTest.java
+++ chapter_005/src/test/java/ru/skilanov/io/list/CyclingLinkedListTest.java
@@ -16,17 +16,35 @@ public class CyclingLinkedListTest {
         CyclingLinkedList list = new CyclingLinkedList();
         CyclingLinkedList.Node first = new CyclingLinkedList.Node(1);
         CyclingLinkedList.Node two = new CyclingLinkedList.Node(2);
-        CyclingLinkedList.Node third = new CyclingLinkedList.Node(3);
+        CyclingLinkedList.Node three = new CyclingLinkedList.Node(3);
         CyclingLinkedList.Node four = new CyclingLinkedList.Node(4);
 
         first.setNext(two);
-        two.setNext(third);
-        third.setNext(four);
+        two.setNext(three);
+        three.setNext(four);
         four.setNext(first);
 
         Assert.assertTrue(list.hasCycle(first));
         Assert.assertTrue(list.hasCycle(two));
-        Assert.assertTrue(list.hasCycle(third));
+        Assert.assertTrue(list.hasCycle(three));
         Assert.assertTrue(list.hasCycle(four));
+    }
+
+    /**
+     * Checking non cycling linked list.
+     */
+    @Test
+    public void wheWeStartCheckNonCyclingThenReturnFalse() {
+        CyclingLinkedList list = new CyclingLinkedList();
+        CyclingLinkedList.Node first = new CyclingLinkedList.Node(1);
+        CyclingLinkedList.Node two = new CyclingLinkedList.Node(2);
+        CyclingLinkedList.Node three = new CyclingLinkedList.Node(3);
+        CyclingLinkedList.Node four = new CyclingLinkedList.Node(4);
+
+        first.setNext(two);
+        two.setNext(three);
+        three.setNext(four);
+
+        Assert.assertFalse(list.hasCycle(four));
     }
 }
